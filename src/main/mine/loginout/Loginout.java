cdd1789ed0eb0c39d8359bad689d4c92cab0e982
ChChristop <105895551+ChChristop@users.noreply.github.com>
2022-05-20 10:51:48 +0900

    Update Loginout.java

--- src/main/mine/loginout/Loginout.java
+++ src/main/mine/loginout/Loginout.java
@@ -2,9 +2,10 @@ package main.mine.loginout;
 
 import java.util.Scanner;
 
+import main.mine.Frame;
+
 import java.util.Hashtable;
 import java.util.Map;
-
 
 class LogInfo{
 	String name;
@@ -26,29 +27,44 @@ class LogInfo{
 	}
 }
 public class Loginout {
+	Map<String, Frame> hT2 = new Hashtable<>();
+	
 	public Loginout() {
-		System.out.println("	로그인 로그아웃 입니다.");
-		Map<String, LogInfo> hT2 = new Hashtable<>();
-		hT2.put("abc", new LogInfo("홍지성",1213,"1234"));
-		hT2.put("bcd", new LogInfo("임지희",1211,"asd"));
+		hT2 =  main.mine.Menu.mDao.getmineDao();
+	}
+	public void run(){
 		String ident;
 		Scanner sc = new Scanner(System.in);
+		
+		if( main.mine.Menu.mDao.getLoginFlag()) {
+			System.out.println("	로그아웃 하시겠습니까?");
+			System.out.println("네(1) 아니오(2)");
+			if(sc.nextInt() == 1) {
+			 main.mine.Menu.mDao.setLoginFlag(false);
+			System.out.println("	로그아웃 되었습니다");
+			}
+			else
+			System.out.println("취소하였습니다.");
+		}
+		System.out.println("	로그인 입니다.");
+
 		do {
 		System.out.println("아이디를 입력하세요.");
-		ident = "abc";
+		ident = sc.nextLine();
 		if(!hT2.containsKey(ident)) {
 			System.out.println("해당하는 아이디가 없습니다.");
 		}
 		}while(!hT2.containsKey(ident));
 
 		System.out.println("비밀번호를 입력하세요.");
-		String pass = sc.next();
+		String pass = sc.nextLine();
 		
 		System.out.println(hT2.get(ident).pass);
 		
-		if(hT2.get(ident).equals(new LogInfo(ident, hT2.get(ident).phoneNum, pass))) {
+		if(hT2.get(ident).pass.compareTo(pass) == 0) {
 			System.out.println(ident + "로 로그인 되었습니다.");
-			//로그인으로 플래그 값 추가할 것
+			 main.mine.Menu.mDao.setLoginFlag(true);
+			 //로그인 정보 저장할 것
 		}
 		else 
 			System.out.println("비밀번호가 일치하지 않습니다.");
