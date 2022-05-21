cace7e3a9e7552b7f0956872ec92a30619be0074
ChChristop <105895551+ChChristop@users.noreply.github.com>
2022-05-21 17:29:23 +0900

    Update Loginout.java

--- src/main/mine/loginout/Loginout.java
+++ src/main/mine/loginout/Loginout.java
@@ -7,41 +7,21 @@ import main.mine.Frame;
 import java.util.Hashtable;
 import java.util.Map;
 
-class LogInfo{
-	String name;
-	int phoneNum;
-	String pass;
-
-	public LogInfo(String name, int phoneNum, String pass) {
-		this.name = name;
-		this.phoneNum = phoneNum;
-		this.pass = pass;
-	}
-	@Override
-	public boolean equals(Object obj) {
-		if (obj instanceof LogInfo) {
-			if (((LogInfo) obj).pass.equals(pass))
-				return true;
-		}
-		return false;
-	}
-}
 public class Loginout {
 	Map<String, Frame> hT2 = new Hashtable<>();
-	
 	public Loginout() {
-		hT2 =  main.mine.Menu.mDao.getmineDao();
 	}
 	public void run(){
+		hT2 =  main.mine.Menu.mDao.getmineDao();
 		String ident;
 		Scanner sc = new Scanner(System.in);
 		
-		if( main.mine.Menu.mDao.getLoginFlag()) {
+		if( main.mine.Menu.mDao.getcuId() != null) {
 			System.out.println("	로그아웃 하시겠습니까?");
 			System.out.println("네(1) 아니오(2)");
 			if(sc.nextInt() == 1) {
-			 main.mine.Menu.mDao.setLoginFlag(false);
-			System.out.println("	로그아웃 되었습니다");
+				main.mine.Menu.mDao.setcuId("");
+				System.out.println("	로그아웃 되었습니다");
 			}
 			else
 			System.out.println("취소하였습니다.");
@@ -59,12 +39,9 @@ public class Loginout {
 		System.out.println("비밀번호를 입력하세요.");
 		String pass = sc.nextLine();
 		
-		System.out.println(hT2.get(ident).pass);
-		
 		if(hT2.get(ident).pass.compareTo(pass) == 0) {
 			System.out.println(ident + "로 로그인 되었습니다.");
-			 main.mine.Menu.mDao.setLoginFlag(true);
-			 //로그인 정보 저장할 것
+			 main.mine.Menu.mDao.setcuId(ident);;//로그인 아이디 정보 저장
 		}
 		else 
 			System.out.println("비밀번호가 일치하지 않습니다.");
