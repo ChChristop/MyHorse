8b271319472215a320a276fa7c114070b2708c55
ChChristop <105895551+ChChristop@users.noreply.github.com>
2022-05-21 17:29:01 +0900

    Update Join.java

--- src/main/mine/join/Join.java
+++ src/main/mine/join/Join.java
@@ -5,24 +5,13 @@ import java.util.Hashtable;
 import java.util.Map;
 
 import main.mine.Frame;
-import main.mine.MineDao;
-
-class JoinInfo extends main.mine.Frame {
-	public JoinInfo(String name, int phoneNum,String pass) {
-		this.name = name;
-		this.phoneNum = phoneNum;
-		this.pass = pass;
-	}
-}
 
 public class Join {
 	Map<String, Frame> hT1 = new Hashtable<>();
-	MineDao mDao = new MineDao();
-	
 	public Join() {
-		hT1 = mDao.getmineDao();
 	}
 	public void run(){
+		hT1 = main.mine.Menu.mDao.getmineDao();
 		String ident;
 		System.out.println("	회원가입 페이지입니다.");
 		
@@ -42,7 +31,8 @@ public class Join {
 		System.out.println("전화번호 뒷 네자리를 입력하세요.");
 		int phoneNum = sc.nextInt();
 
-		hT1.put(ident, new JoinInfo(name,phoneNum,pass)); //아이디 생성
+		hT1.put(ident, new Frame(name,phoneNum,pass)); //아이디 생성
+		main.mine.Menu.mDao.setmineDao(hT1);//동기화
 		System.out.println(ident + "아이디가 생성되었습니다.");
 		System.out.println("이름: "+ name + ", 비밀번호: " + pass + ", 전화번호 뒷 4자리: " + phoneNum);
 		
