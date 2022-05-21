decc44b07ac83788d0fc5347134c78f2a2d1f05e
ChChristop <105895551+ChChristop@users.noreply.github.com>
2022-05-21 17:17:48 +0900

    Update Menu.java

--- src/main/events/cuEvent/Menu.java
+++ src/main/events/cuEvent/Menu.java
@@ -2,52 +2,42 @@ package main.events.cuEvent;
 
 import java.util.ArrayList;
 import java.util.Scanner;
-
-class EventList{
-	int number;
-	String title;
-	EventList(){
-	}
-	EventList(int number, String title){
-		this.number =number;
-		this.title =title;
-	}
-}
+import main.events.EventList;
 
 public class Menu{
 	ArrayList<EventList> cuEventList = new ArrayList<>();
 	
-//	cuEventList.add(new EventList(1,"출석 체크"));
-//	cuEventList.add(new EventList(2,"포인트 2배 이벤트"));
-//	
 	Scanner sc = new Scanner(System.in);
-
-
+	DailyCheck dcheck = new DailyCheck();
+	public static Point2X p2x = new Point2X();
+	
 	public void run() {
 		int menu=0;
 		
-	System.out.println("	진행중인 이벤트 목록");
-//	
-//	for(EventList clist:cuEventList) {
-//		System.out.println(clist.number +"." + clist.title);
-//	}
+		cuEventList = main.events.Menu.fe.getCuEventList();
+		
+		System.out.println("	진행중인 이벤트 목록");
+
+		for(EventList clist:cuEventList) {
+			System.out.println(clist.number +"." + clist.title);
+		}
+	
 	while(menu >= 0) {
-		System.out.println("출석체크 (1), 포인트 2배 이벤트 (2) 뒤로가기 (3)");
-//		for(EventList clist:cuEventList) {
-//		System.out.print(clist.title +" (" + clist.number +") ");
-//		}
-//		System.out.println("뒤로가기 (" + (1 + cuEventList.size())+")");
+//		System.out.println("출석체크 (1), 포인트 2배 이벤트 (2) 뒤로가기 (3)");
+
+		for(EventList clist:cuEventList) {
+		System.out.print(clist.title +" (" + clist.number +") ");
+		}
+		System.out.println("뒤로가기 (" + (1 + cuEventList.size())+")");
 		
 		menu = sc.nextInt();
 		
 		switch(menu) {
 		case 1:
-			new CalcCheck();
+			dcheck.run();
 			break;
 		case 2:
-			System.out.println("	포인트 2배 이벤트");
-			System.out.println("홀수일 획득 포인트 2배 이벤트");
-//			new main.events.endEvent.Menu();
+			p2x.run();
 			break;
 		case 3:
 			System.out.println("	이전으로 이동합니다.	");
