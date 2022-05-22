93b181a33193debb515a23fa9c391da0a1fb591c
Rhaun95 <105895293+Rhaun95@users.noreply.github.com>
2022-05-22 21:07:10 +0900

    Update GameDao.java

--- src/main/game/GameDao.java
+++ src/main/game/GameDao.java
@@ -5,51 +5,14 @@ import main.ingame.horse.RaceHorse;
 import main.ingame.race.SaveRaces;
 
 public class GameDao {
-	private Map<String, Frame> mineDao = new Hashtable<>();
-	private String cuId;
-  
 	SaveRaces sr = new SaveRaces();
 	RaceHorse rh = new RaceHorse();
+	
 	ArrayList<ArrayList<RaceHorse>> resultDao = new ArrayList<>();
 	ArrayList<RaceHorse> stallDao = new ArrayList<>();
 	int field;
-  
-	
-	public MineDao(){
-		mineDao.put("abc", new Frame("홍지성",1213,"1234"));
-		mineDao.put("bcd", new Frame("임지희",1211,"asd"));
-	}
-	public Map<String, Frame> getmineDao(){
-		return mineDao;
-	}
-	public void setmineDao(Map<String, Frame> mineDao){
-		this.mineDao = mineDao;
-	}
-	public String getcuId(){
-		return cuId;
-	}
-	public void setcuId(String cuId){
-		this.cuId = cuId;
-	}
-	public Frame getFrame(){
-		return mineDao.get(cuId);
-	}
-	public void setDcheck(boolean dcheck){
-		mineDao.get(cuId).setDcheck(dcheck);
-		mineDao.replace(cuId, mineDao.get(cuId));
-	}
-	public boolean getDcheck(){
-		return mineDao.get(cuId).dcheck;
-	}
-	public void setPoint(int point){
-		mineDao.get(cuId).setPoint(point);
-		mineDao.replace(cuId, mineDao.get(cuId));
-	}
-	public int getPoint(){
-		return mineDao.get(cuId).point;
-
-  
-public void setResultDao(ArrayList<ArrayList<RaceHorse>> result) {
+			
+	public void setResultDao(ArrayList<ArrayList<RaceHorse>> result) {
 		this.resultDao =result ;
 	}
 	public ArrayList<ArrayList<RaceHorse>> getResultDao(){
@@ -72,7 +35,18 @@ public void setResultDao(ArrayList<ArrayList<RaceHorse>> result) {
 	public void setField(int field) {
 		this.field = field;
 	}
-  
-  
+	
+	
+	public void print(ArrayList<ArrayList<RaceHorse>> resultDao) {
+		for(ArrayList<RaceHorse> race :resultDao ) {
+			for(RaceHorse horse : race) {
+				System.out.print("이름: " + horse.getName());
+				System.out.print("순위: " + horse.getRank() + "위" );
+				System.out.println(horse.getLine());
+			}
+		}
+		
 	}
+	
+	
 }
