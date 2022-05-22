431ea4d27868b90cb01902f2a3028e13e31226b0
kkumdol2 <105897652+kkumdol2@users.noreply.github.com>
2022-05-22 11:12:07 +0900

    Update Item.java

--- src/main/ingame/horse/Item.java
+++ src/main/ingame/horse/Item.java
@@ -18,18 +18,16 @@ public class Item {
 		h.addStamina(20);
 	} 
 
-// 수현씨~ 이거 잘 모르겠어요.
-// 말 랜덤 생성한거 불러서 ?? addSpeed(-20) 해줘야될거 같은데 
-// r.createRandomNumber2().addSpeed(-20) 이렇게 하면 안되네염. ㅠㅠ 
-//	public void useAttackItem(RaceHorse r) {
-//		r.createRandomNumber2();
-//	}
+
+	public void useAttackItem(ArrayList<RaceHorse> raceHorses) {
+		int a = r.createRandomNumber2();
+		raceHorses.get(a).addSpeed(-20);
+		
+	}
 	
 	public void useExpItem(MyHorse h) {
-		h.addExp(100);
-		//아이템 썼을때 경험치가 올라도 레벨이 안올라요.
-		//ingame에서 u.levelUp(h.getMyHorses().get(0)); 시도해봣는데 안됩니다
-		//(거기도 주석처리 해둠)
+		h.addExp(100); 
+		u.levelUp(h);
 		
 	}	
 }
