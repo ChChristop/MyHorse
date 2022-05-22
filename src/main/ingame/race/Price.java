5b669a45839fcc3adb70e0ed07ab8d6afc892729
Rhaun95 <105895293+Rhaun95@users.noreply.github.com>
2022-05-22 21:09:27 +0900

    Update Price.java

--- src/main/ingame/race/Price.java
+++ src/main/ingame/race/Price.java
@@ -1,9 +1,7 @@
 package main.ingame.race;
 
-import java.util.ArrayList;
-import java.util.Scanner;
 
-import main.ingame.horse.MyHorse;
+import java.util.Scanner;
 import main.ingame.horse.RaceHorse;
 /*게임 보상	
  * 상금받기(말주인)	
@@ -13,20 +11,14 @@ import main.ingame.horse.RaceHorse;
 interface Get{
 	
 	void victoryPrice(RaceHorse rh);
-	void afterGameMoney(int n);
 	void afterGameExp(RaceHorse rh);
 	
 }
 
 public class Price implements Get{
-	ArrayList<RaceHorse> raceHorces = new ArrayList<RaceHorse>();
+	
 	RaceHorse r = new RaceHorse();
-	
 	Scanner sc = new Scanner(System.in);
-
-	int betPersons=7;
-	int betTotal = 7000;
-	
 
 	//1등 상금받기(말주인)
 	public void victoryPrice(RaceHorse rh) {
@@ -36,11 +28,6 @@ public class Price implements Get{
 			System.out.println(rh.getLine()+" " + rh.getName() + ": 다음 기회를 노려보세요.");
 	}
 	
-	//배팅금 받기(배팅자)
-	public void afterGameMoney(int n) {//n = 배팅한 총 사람 수
-		System.out.println("배팅금 total/n을 획득하셧습니다.");
-	
-	}
 
 	//말에게 경험치 주기
 	//1-3등까지만 경험치 추가
