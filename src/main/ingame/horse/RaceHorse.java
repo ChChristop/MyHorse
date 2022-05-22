127633a36688254c59605d64dcb77fb39a0f4a07
ChChristop <105895551+ChChristop@users.noreply.github.com>
2022-05-22 15:20:07 +0900

    Update RaceHorse.java

--- src/main/ingame/horse/RaceHorse.java
+++ src/main/ingame/horse/RaceHorse.java
@@ -250,23 +250,26 @@ public class RaceHorse{
 		
 		//현재 거리
 		int count = 0;
-		int mass = (int) (count * rh.getSpeed());
+		int mass = 0;
 		
-		int oneStep = fieldLength/40;
-		//한칸 이동
-		int stone = mass/oneStep; 
-		
+		int oneStep = fieldLength/40;	// 2500/40 = 62.5 = 1칸 정보
+		int stone = 0; 	//현재 위치 정보
+		System.out.println();
 		while(stone < 40) {
 			System.out.print(rh.getName()+ ": ");
 			for(int j = 0 ; j<40; j++) {
-				
+				if(stone == j ) 
+				fieldStone[stone] = ">";
+				else
 				fieldStone[j] = "_ ";
-				if(stone == j ) fieldStone[stone] = ">";
 				System.out.print(fieldStone[j]);
 			}
+			mass = (int) (count * rh.getSpeed());
+			stone = mass/oneStep;
 			count++;// _ 40개
 			rh.setRank(this.rank);
 			this.rank++;	
+			System.out.println();
 			}
 	}
 		
